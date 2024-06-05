29b24eae8a814ce470c757c100a3789fda589a8e
Abhishek Mukherjee <mukherjeeabhishek207@gmail.com>
2024-06-05 14:52:01 +0530

    add welcome message and a card below the welcome message

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const TotalBalanceBox = ({
+  accounts = [], 
+  totalBanks , 
+  totalCurrentBalance
+}:TotalBalanceBoxProps) => {
+  return (
+    <section className='total-balance'>TotalBalanceBox</section>
+  );
+};
+
+export default TotalBalanceBox
